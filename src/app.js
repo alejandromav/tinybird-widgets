a10b39c9a07fedeb24b160af773badec625bb5c0
Alejandro Martin <alejandromav@tinybird.co>
2022-08-07 18:34:34 +0200

    Add service worker

--- src/app.js
+++ src/app.js
@@ -2,4 +2,13 @@ import Card from './components/card';
 
 (async () => {
     customElements.define('tb-card', Card);
+    // Detect if service workers enabled
+    if ('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register(new URL('service-worker.js', import.meta.url),{type: 'module'});
+            console.log('Service Worker Registered');
+        } catch (error) {
+            console.log('Service Worker Register Failed');
+        }
+    }
 })();
